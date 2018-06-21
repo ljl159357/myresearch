14ef9db5d5848e3240ad27f2777b957a57c3db58
ljl159357 <40022224+ljl159357@users.noreply.github.com>
2018-06-21 18:46:46 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -31,7 +31,7 @@ echo "############"pipeline_config_path begin" #################"
 tail -50  $pipeline_config_path
 echo "############"pipeline_config_path end" #################"
 
-for i in {0..4}  # for循环中的代码执行5此，这里的左右边界都包含，也就是一共训练500个step，每100step验证一次
+for i in {0..9}  # for循环中的代码执行5此，这里的左右边界都包含，也就是一共训练500个step，每100step验证一次
 do
     echo "############" $i "runnning #################"
     last=$[$i*100]
